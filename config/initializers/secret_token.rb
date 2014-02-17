ec2f54c76de19043ef4009c90af545faff931388
John Dean <jbd7899@me.com>
2014-02-16 23:07:36 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Textbookvalet::Application.config.secret_key_base = '40278ddcbf0f35c77bbb991d085c9d873026c931f1bf5a2d628663bd3d3e1f273ed8c10c8bab71bd7b2cf551704f1912ca24026f09690b77160bc0b78a397388'
